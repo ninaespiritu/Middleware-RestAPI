e5470d54742b6ee1127ffa8be32b2a726b82a09c
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-02-17 15:29:24 +0000

    Build PATCH route: update User email or username

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs/dist/bcrypt");
+const req = require("express/lib/request");
 const User = require("./userModel");
 
 // ADD USER
@@ -22,7 +23,37 @@ exports.findUser = async (req, res) => {
 			const getUser = await User.findOne({ username: req.body.username });
 			res.status(200).send({ user: getUser });
 		} else {
-			res.status(400).send({ message: "Invalid request" })
+			res.status(400).send({ message: "Invalid request" });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({ err: error.message });
+	}
+};
+
+// UPDATE USER
+exports.updateUser = async (req, res) => {
+	try {
+		if (req.body.newemail) {
+			const amendUser = await User.updateOne(
+				{ email: req.body.email },
+				{ $set: { email: req.body.newemail } }
+			);
+			res.status(200).send({
+				amendUser,
+				updateUser: `User email updated to: ${req.body.newemail}`,
+			});
+		} else if (req.body.newusername) {
+			const amendUser = await User.updateOne(
+				{ username: req.body.username },
+				{ $set: { username: req.body.newusername } }
+			);
+			res.status(200).send({
+				amendUser,
+				updateUser: `User username updated to: ${req.body.newusername}`,
+			});
+		} else {
+			res.status(400).send({ message: "Invalid request" });
 		}
 	} catch (error) {
 		console.log(error);
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const { hashPassword } = require("../middleware");
-const { addUser, logIn, findUser } = require("./userControllers");
+const { addUser, logIn, findUser, updateUser } = require("./userControllers");
 const userRouter = Router();
 
 // User routes
 userRouter.post("/user", hashPassword, addUser);
 userRouter.get("/user", findUser);
+userRouter.patch("/user", updateUser);
 
 // Login route
 userRouter.post("/login", logIn);
